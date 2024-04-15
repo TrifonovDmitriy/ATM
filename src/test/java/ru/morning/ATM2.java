bc0f11728fcd8d50b6a662a007af5f2b891c024a
Trifonov Dmitriy <dmitriy@it-qa.ru>
2024-04-15 23:43:02 +0700

    atm 2

---
+++ src/test/java/ru/morning/ATM2.java
@@ -0,0 +1,14 @@
+package ru.morning;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class ATM2 {
+    private Map<Bills,Integer> cash;
+
+    public ATM2(Map<Bills, Integer> cash) {
+        this.cash = new LinkedHashMap<>;
+        cash.forEach((K,V)->{});
+    }
+
+}
