be47a9e8d4ead1f09b2b5e17def1f8377f8070d7
Trifonov Dmitriy <dmitriy@it-qa.ru>
2024-04-15 14:00:11 +0700

    second commit

--- src/test/java/ru/morning/ATM.java
+++ src/test/java/ru/morning/ATM.java
@@ -89,27 +89,3 @@ public class ATM {
     }
 }
 
-class App {
-    public static void main(String[] args) {
-        // проверка методов пополения и получения баланса
-        ATM atm = new ATM(10, 10, 10);
-        atm.addMoney(20, 5, 10);
-        atm.getBalanse();
-        System.out.println("************************************");
-
-        //проверяем метод снятия средств (успешно)
-        atm.getMoney(1330);
-        atm.getBalanse();
-        System.out.println("************************************");
-
-        //Проверка не кратности номинала 20
-        atm.getMoney(105);
-        atm.getBalanse();
-        System.out.println("************************************");
-
-        //проверка недостаточности средств в банкомате
-        ATM atm2 = new ATM(1, 5, 100);
-        atm2.getMoney(290);
-    }
-}
-
